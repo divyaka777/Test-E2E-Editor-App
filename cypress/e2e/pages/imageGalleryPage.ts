5d0d12bdfb05c6b2851a6edbe0708b60cd12ee1b
divyaka777 <divya.kadeekal@coachhub.io>
2024-07-02 16:07:27 +0200

    feat: image element test

--- cypress/e2e/pages/imageGalleryPage.ts
+++ cypress/e2e/pages/imageGalleryPage.ts
@@ -3,6 +3,29 @@ class ImageGalleryPage
     visit(){
         cy.visit('/')
     }
+
+    getHomeText() {
+        return cy.contains('Home')
+      }
+    getImageElement() {
+        return cy.get('img[alt="Image by Alejandro Escamilla"]')
+      }
+    getWidthInput() {
+        return cy.get('#width-input')
+      }
+    getHeightInput() {
+        return cy.get('#height-input')
+      }
+    getDownloadImageElement() {
+        return cy.contains('font', 'Download Image')
+      }
+
+    getDiscardChangesElement() {
+        return cy.contains('font', 'Discard Changes')
+      }
+    
+    
+    
 }
 
 export const imageGalleryPage = new ImageGalleryPage();
--- cypress/e2e/tests/image-browser-page.cy.ts
+++ cypress/e2e/tests/image-browser-page.cy.ts
@@ -1,8 +1,30 @@
 import { imageGalleryPage } from "../pages/imageGalleryPage"
 
-describe('Visit HomePage', () => {
-    it('Homepage should load',() =>
-    {
-        imageGalleryPage.visit();
+describe('Image Element Test', () => {
+    beforeEach(() => {
+      // Visit the page before each test
+      imageGalleryPage.visit();
     })
-})
+    
+    it('should display the home page', () => {
+        // Check if the homepage is loaded
+        imageGalleryPage.getHomeText().should('be.visible');
+      })
+
+  
+    it('should load the image element', () => {
+      // Check if the image element is present and visible
+      imageGalleryPage.getImageElement().should('be.visible');
+
+    })
+    
+  
+    it('should have the correct src attribute', () => {
+      // Check if the image element has the correct src attribute
+      cy.get('img[alt="Image by Alejandro Escamilla"]')
+        .should('have.attr', 'src')
+        .and('include', 'https://picsum.photos/id/0/5000/3333')
+    })
+  
+  })
+  
