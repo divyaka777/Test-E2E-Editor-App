d5c5ff0541b02e0544d92ffb6ce3c2f71f8a7f7d
divyaka777 <divya.kadeekal@coachhub.io>
2024-07-08 21:42:35 +0200

    feat: Refactor test cases

--- cypress/e2e/pages/imageGalleryPage.ts
+++ cypress/e2e/pages/imageGalleryPage.ts
@@ -11,25 +11,7 @@ class ImageGalleryPage
         
         return cy.get('img').eq(index)
       }
-    getWidthInput() {
-        return cy.get('#width-input')
-      }
-    getHeightInput() {
-        return cy.get('#height-input')
-      }
-    getGreyScale()
-      {
-        return cy.get ('[id="greyscale-input"]')
-      }
-    getDownloadImageElement() {
-        return cy.get('[aria-label="Download edited image"]')
-      }
-    getBlurScaleElement(){
-        return cy.get('[id="blur-input"]').invoke('val',5)
-    }
-    getDiscardChangesElement() {
-        return cy.get('[aria-label="Discard changes to the image"]')
-      }
+    
     
     
     
--- cypress/e2e/tests/image-browser-page.cy.ts
+++ cypress/e2e/tests/image-browser-page.cy.ts
@@ -1,3 +1,4 @@
+import { imageEditorPage } from "../pages/imageEditorPage";
 import { imageGalleryPage } from "../pages/imageGalleryPage"
 
 describe('Image Element Test', () => {
@@ -21,16 +22,16 @@ describe('Image Element Test', () => {
     it('should be able to dowload the image', () => {
         //Check if the image is visible and downloadable
         imageGalleryPage.getImageElement().should('be.visible').click();
-        imageGalleryPage.getDownloadImageElement().should('be.visible').click();
+        imageEditorPage.getDownloadImageElement().should('be.visible').dblclick();
 
     })
 
     it('should be able to add greyscale changes to the image and discard them', () => {
         //Check if the image is visible and can introduce greyscale changes 
         imageGalleryPage.getImageElement().should('be.visible').click();
-        imageGalleryPage.getWidthInput().type('100');
-        imageGalleryPage.getGreyScale().check();
-        imageGalleryPage.getDiscardChangesElement().should('be.visible').click();
+        imageEditorPage.getWidthInput().type('100');
+        imageEditorPage.getGreyScale().check();
+        imageEditorPage.getDiscardChangesElement().should('be.visible').click();
 
 
     })
@@ -38,27 +39,27 @@ describe('Image Element Test', () => {
     it('should be able to edit changes to the image', () => {
         //Check if the image is visible and editable and downloadable
         imageGalleryPage.getImageElement().should('be.visible').click();
-        imageGalleryPage.getWidthInput().type('100');
-        imageGalleryPage.getHeightInput().type('400');
-        imageGalleryPage.getDownloadImageElement().should('be.visible').click();
+        imageEditorPage.getWidthInput().clear().type('100');
+        imageEditorPage.getHeightInput().type('400');
+        imageEditorPage.getDownloadImageElement().should('be.visible').dblclick();
 
     })
 
     it('should be able to add blur to the image ', () => {
         //Check if the image is visible and could be blurred to a specific value
         imageGalleryPage.getImageElement().should('be.visible').click();
-        imageGalleryPage.getBlurScaleElement().should('have.value','5');
+        imageEditorPage.getBlurScaleElement().should('have.value','5');
 
     })
 
     it('should be able to discard changes to the image', () => {
         //Check if the image is visible and editable and could discard the changes
         imageGalleryPage.getImageElement().should('be.visible').click();
-        imageGalleryPage.getWidthInput().type('100');
-        imageGalleryPage.getHeightInput().type('400');
-        imageGalleryPage.getGreyScale().check();
-        imageGalleryPage.getBlurScaleElement().should('have.value','5');
-        imageGalleryPage.getDiscardChangesElement().should('be.visible').click();
+        imageEditorPage.getWidthInput().type('100');
+        imageEditorPage.getHeightInput().type('400');
+        imageEditorPage.getGreyScale().check();
+        imageEditorPage.getBlurScaleElement().should('have.value','5');
+        imageEditorPage.getDiscardChangesElement().should('be.visible').click();
 
     })
     
